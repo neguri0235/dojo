03c2772a9ce1eb1806e195e9f771156d12b58466
neguri <projekct@gmail.com>
2018-08-01 21:52:49 +0900

    2*n tile

---
+++ boj/dp/11727.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+long d[1000];
+
+int n;
+
+int main()
+{
+
+    std::cin>>n;
+
+    d[0] = 1;
+    d[1] = 1;
+
+
+    for(int i = 2; i<=n ; i++){
+        d[i] =( d[i-1] + d[i-2] + d[i-2])%10007;
+    }
+
+
+    std::cout<<d[n]<<std::endl;
+
+    return 0;
+}
