397896d8ce792fd76e18e29864219bd552cc8814
neguri <projekct@gmail.com>
2018-08-16 21:06:33 +0900

    remove dp

--- boj/1463.cpp
+++ boj/1463.cpp
@@ -1,65 +1,57 @@
-//www.acmicpc.net/problem/1463
+//dp. top-down 방식으로 
 
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-
-#define DBG 0
 
 int n;
-int sol;
+int d[1000001];
 
-void solve();
+
+
+int f(int a)
+{
+
+
+    if(a == 1) return 0;
+
+    if(d[a] > 0) return d[a];
+
+    d[a] = f(a-1) +1;
+
+    // 0 1 2 3 4 5 6 7 8 9 
+
+    if(a%2 == 0) {
+    //    std::cout<<"DBG: "<<a<<std::endl;
+        d[a] = std::min(f(a/2)+1, d[a]);
+    }
+
+    // 0 1 2 2 3 3 4 3 4 4 
+
+
+    if(a%3 == 0) {
+        d[a] = std::min(f(a/3)+1, d[a]);
+    }
+
+    // 0 1 1 2 3 2 3 3 2 3
+
+    return d[a];
+
+
+}
+
 
 int main()
 {
-    std::cin>>n;
-    int start = clock();
 
-    solve();
-    std::cout<<sol<<std::endl;
-    if(DBG) std::cout<<(double)(clock()-start)/CLOCKS_PER_SEC<<std::endl;
+    std::cin>>n;
+
+  
+    std::cout<<f(n)<<std::endl;
+
+    for(int i = 0; i<11; i++) {
+
+        std::cout<<d[i]<<" ";
+    }
+    std::cout<<std::endl;
 
     return 0;
 }
-
-void solve()
-{
-    std::vector<int> v(n);
-    
-    std::queue<int>sq;         //small queue
-    std::queue<std::queue<int>>lq; //large queue
-
-    sq.push(n);
-    lq.push(sq);
-
-    while(lq.empty()==false) {
-        auto ld = lq.front(); lq.pop(); //large data
-        size_t sz = ld.size();
-
-        while(sz--) {
-
-            auto sd = ld.front(); ld.pop(); //small data
-
-            if(sd == 1) return;
-
-            if(v[sd-1] == 0) {
-                v[sd-1] = 1; ld.push(sd-1);
-            }
-
-            if(v[sd/3] == 0 && sd%3 == 0) {
-                v[sd/3] = 1; ld.push(sd/3);
-            }
-
-            if(v[sd/2] == 0 && sd%2 == 0) {
-                v[sd/2] = 1; ld.push(sd/2);
-            }
-
-        }
-        lq.push(ld);
-        sol++;
-    }
-}
-
