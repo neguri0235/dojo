ac0ac5af129891bef6773329249a05fa05542c78
neguri <projekct@gmail.com>
2018-08-22 21:38:05 +0900

    coin 2

--- boj/2294.cpp
+++ boj/2294.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include <cstdio>
 
+using namespace std;
+
 int n,k;
 
-int a[101];
-int d[10001];
+int a[102];
+long long d[100002];
 
 int main()
 {
-    std::cin>>n>>k;
 
-    for(int i = 1; i<=n; i++){
-        scanf("%d",&a[i]);
-    }
+    cin>>n>>k;
 
+    for(int i = 0; i<n; i++) cin>>a[i];
 
+    for(int i = 1; i<=k; i++) d[i] = 987654321;
 
-    for(int i = 0; i<=k; i++) {
-        d[i] = 100001;
-    }
-
-    d[0] = 0;
-
-    for(int i = 1; i<=n; i++){
+    for(int i = 0; i<n; i++){
 
         for(int j = a[i]; j<=k; j++){
-
-            d[j] = std::min(d[j], d[j-a[i]]+1);
+            d[j] = std::min(d[j], d[j-a[i]] + 1);
         }
     }
 
-    if(d[k] ==100001){
-        std::cout<<-1<<std::endl;
-    } else{
-        std::cout<<d[k]<<std::endl;
-    }
+    if(d[k] == 987654321) cout<<"impossible"<<endl;
+    else cout<<d[k]<<endl;
 
     return 0;
 }
