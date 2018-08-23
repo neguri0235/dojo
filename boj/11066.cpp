58730bbf5ff48f086121fcaac59a79de837e52e7
neguri <projekct@gmail.com>
2018-08-23 19:32:56 +0900

    file merge

--- boj/11066.cpp
+++ boj/11066.cpp
@@ -2,23 +2,49 @@
 #include <cstring>
 #include <cstdio>
 
+using namespace std;
+
 int tc;
 int n;
 int a[502];
-int d[10002];
+int s[502];
+int d[502][502];
 
+
+int go(int i, int j)
+{
+
+    if(i == j) return 0;
+
+    if(d[i][j] != -1) return d[i][j];
+
+    int &ans = d[i][j];
+
+    for(int k = i; k<=j-1; k++){
+
+        int temp = go(i,k) + go(k+1,j) + s[j] - s[i-1];
+
+        if( ans == -1 || ans > temp) ans = temp;
+    }
+
+    return ans;
+}
 
 int main()
 {
     cin>>tc;
     while(tc--){
         cin>>n;
-        for(int i = 0; i<n; i++) cin>>a[i];
+
         memset(d,-1, sizeof(d));
+        memset(s,0,sizeof(s));
 
+        for(int i = 1; i<=n; i++){
+            cin>>a[i];
+            s[i] = s[i-1] + a[i];
+        }
 
-       
+        cout<<go(1,n)<<endl;
     }
-    
     return 0;
 }
