bf8ad28f5ebb30df80f310a199133103a1da1d56
neguri <projekct@gmail.com>
2018-08-23 20:31:23 +0900

    race

--- jungol/1491.cpp
+++ jungol/1491.cpp
@@ -1,52 +1,47 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int L;
-int n;
-int a[102];
-int t[102];
-
-int d[102];
-int r[102];
-
-int cnt;
-int idx;
+int L,n,a[102],t[102],d[102],p[102],o,last,cnt;
 
 void print(int i)
 {
-    if(i == 0){
+    //cout<<"D: "<<i<<" "<<p[i]<<endl;
+    if( i == 0){
         cout<<cnt<<endl;
-        return ;
+        return;
     }
 
-    cnt = cnt+1;
-    print(r[i]);
+    cnt++;
+
+    print(p[i]);
     cout<<i<<" ";
 }
-
 
 int main()
 {
     cin>>L;
     cin>>n;
-    for(int i = 1; i<=n+1; i++){int z;  cin>>z; a[i] = z + a[i-1];}
-    for(int i = 1; i<=n; i++) cin>>t[i];
 
-    for(int i = 1; i<102; i++) d[i] = 987654;
+    for(int i= 1; i<=n+1; i++){
+        cin>>o;
+        a[i] = a[i-1] + o;
+        d[i] = 987654321;
+    }
+
+    for(int i = 1; i<=n;i++){
+        cin>>t[i];
+    }
 
     d[0] = 0;
-
-
     for(int i = 1; i<=n+1; i++) {
-        for(int j = i-1; j >=0; j--){
-
+        for(int j = i-1; j >= 0; j--) {
+            
             if(a[i] - a[j] <= L) {
-                if(d[j] + t[i] < d[i]){
+                if(d[j] + t[i] < d[i] ){
                     d[i] = d[j] + t[i];
-                    r[i] = j;
-                    idx = j;
+                    p[i] = j;
+                    last = j;
                 }
             }
         }
@@ -54,27 +49,15 @@ int main()
 
     cout<<d[n+1]<<endl;
 
-#define DBG 0
+    if(d[n+1] == 0) return 0;
 
-    int pr = -1;
+    //for(int i = 1; i<=n+1; i++) cout<<p[i]<<" ";
+    //cout<<endl;
 
-#if DBG
+    //cout<<"last: "<<last<<endl;
+    print(last);
     
-    for(int i = 0; i<=n+1; i++){
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
-
-    for(int i = 0; i<n+1; i++){
-        cout<<d[i]<<" ";
-    }
-    cout<<endl;
-
-
-#endif
-
-    print(idx);
-
-
     return 0;
 }
+
+
