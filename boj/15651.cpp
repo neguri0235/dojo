615f68bac8e31aa9db04ce44e08a7057d78baa35
최재호/선임연구원/VC스마트연구담당(elliott.choi@lge.com) <elliott.choi@lge.com>
2018-11-05 20:59:22 +0900

    n&m

---
+++ boj/15651.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n,m;
+
+int check[10];
+vector<int> v;
+
+
+void go()
+{
+
+    if(v.size() == m) {
+        for(int i = 0; i<m; i++) {
+            cout<<v[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+
+    for(int i = 0; i<n; i++) {
+
+        //if(check[i+1]) continue;
+
+        check[i+1] = 1;
+        v.push_back(i+1);
+        go();
+        check[i+1] = 0;
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin>>n>>m;
+    go();
+    return 0;
+}
