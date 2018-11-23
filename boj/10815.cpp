5bcd74b11124f996e1e7c97845152cf6889b71fe
최재호/선임연구원/VC스마트연구담당(elliott.choi@lge.com) <elliott.choi@lge.com>
2018-11-23 21:04:47 +0900

    fix timeout

--- boj/10815.cpp
+++ boj/10815.cpp
@@ -8,6 +8,31 @@ int n,t;
 long a[500001];
 long c;
 
+int go(long f)
+{
+
+    int s = 0;
+    int e = n;
+    int m = (s+n)/2;
+
+    while( s < e ) {
+
+       // cout<<s<<" "<<e<<" "<<m<<endl;
+
+        if(a[m] == f) return 1;
+        
+        if( a[m] > f) {
+            e = m;
+        }else {
+            s = m+1;
+        }
+        m = (s+e)/2;
+    }
+
+    return 0;
+
+}
+
 int main()
 {
     scanf("%d",&n);
@@ -21,10 +46,13 @@ int main()
 
     for(int i = 0; i<t; i++){
         scanf("%ld",&c);
+        /*
 
         auto r = find(begin(a),begin(a)+n,c);
         if(r != begin(a)+n) printf("1 ");
         else printf("0 ");
+        */
+        printf("%d ",go(c));
     }
 
 
