3090ef48f72aa10e56e19e033b991567911762c8
최재호/선임연구원/VC스마트연구담당(elliott.choi@lge.com) <elliott.choi@lge.com>
2018-11-30 19:48:11 +0900

    01

---
+++ boj/1904.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int d[1000001];
+
+int main()
+{
+    int n;
+    cin>>n;
+    d[1] = 1;
+    d[2] = 2;
+    for(int i = 3; i<=n; i++){
+        d[i] =(d[i-1] + d[i-2]) %15746;
+    }
+
+    cout<<d[n]<<'\n';
+    return 0;
+}
