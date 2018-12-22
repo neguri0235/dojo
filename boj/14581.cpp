53d48fbde7e632356a1327db701fd395198e1087
neguri <projekct@gmail.com>
2018-12-22 23:24:17 +0900

    hj

---
+++ boj/14581.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<":fan::fan::fan:"<<endl;
+    cout<<":fan::"<<s<<"::fan:"<<endl;
+    cout<<":fan::fan::fan:"<<endl;
+
+    return 0;
+}
