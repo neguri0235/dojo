832a743db8f9011446f977d5fba0c254a903f98a
jaehoc <projekct@gmail.com>
2019-09-11 21:26:39 +0900

    argument upate

--- pyKata/arg.py
+++ pyKata/arg.py
@@ -1,7 +1,188 @@
+
+'''
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("echo")
-parser.parse_args()
-
 args = parser.parse_args()
 print(args.echo)
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number", type=int)
+args = parser.parse_args()
+print(args.square**2)
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("first")
+parser.add_argument("second")
+args = parser.parse_args()
+print(args.first)
+print(args.second)
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbosity", help="increase output verbosity")
+args = parser.parse_args()
+if args.verbosity:
+    print("verbosity turned on")
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbose:
+    print("the square of {} equals {}".format(args.square, answer))
+else:
+    print(answer)
+
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", type=int,
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print("the square of {} equals {}".format(args.square, answer))
+elif args.verbosity == 1:
+    print("{}^2 == {}".format(args.square, answer))
+else:
+    print(answer)
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print("the square of {} equals {}".format(args.square, answer))
+elif args.verbosity == 1:
+    print("{}^2 == {}".format(args.square, answer))
+else:
+    print(answer)
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display the square of a given number")
+parser.add_argument("-v", "--verbosity", action="count",
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print("the square of {} equals {}".format(args.square, answer))
+elif args.verbosity == 1:
+    print("{}^2 == {}".format(args.square, answer))
+else:
+    print(answer)
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("-v", "--verbosity", action="count", default=0)
+args = parser.parse_args()
+answer = args.x**args.y
+if args.verbosity >= 2:
+    print("{} to the power {} equals {}".format(args.x, args.y, answer))
+elif args.verbosity >= 1:
+    print("{}^{} == {}".format(args.x, args.y, answer))
+else:
+    print(answer)
+
+'''
+
+'''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("-v", "--verbosity", action="count", default=0)
+args = parser.parse_args()
+answer = args.x**args.y
+if args.verbosity >= 2:
+    print("Running '{}'".format(__file__))
+if args.verbosity >= 1:
+    print("{}^{} == ".format(args.x, args.y), end="")
+print(answer)
+
+'''
+
+'''
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print("{} to the power {} equals {}".format(args.x, args.y, answer))
+else:
+    print("{}^{} == {}".format(args.x, args.y, answer))
+
+
+'''
+import argparse
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print("{} to the power {} equals {}".format(args.x, args.y, answer))
+else:
+    print("{}^{} == {}".format(args.x, args.y, answer))
