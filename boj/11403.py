98456610ebac342c80d21218d0c65b3eb9e0e0a9
neguri <projekct@gmail.com>
2020-01-07 23:38:42 +0900

    dfs

--- boj/11403.py
+++ boj/11403.py
@@ -1,9 +1,8 @@
 import sys
 import logging
-import collections
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s = %(levelname)s - %(message)s')
-#logging.disable(logging.DEBUG)
+logging.disable(logging.DEBUG)
 
 A = [ ]
 D = [ [0 for i in range(101)] for j in range(101)]
@@ -14,12 +13,22 @@ for i in range(n):
     data = list(map(int, sys.stdin.readline().split()))
     A.append(data)
 
-logging.debug(A)
+for e in A:
+    logging.debug(e)
 
-d = collections.deque()
+def go(c):
+    for i in range(n):
+        if A[c][i] == 1 and V[i] == 0:
+            V[i] = 1
+            go(i)
+
 for i in range(n):
-    d.append(1)
-    while d.count():
-        next = d.popleft()
-        D[i][next] = 1
-        d.append(next)
+    V = [0]*n
+    for j in range(n):
+        if A[i][j]:
+            V[j] = 1
+            go(j)
+    logging.debug(V)
+    for e in V:
+        print(e, end = ' ')
+    print()
