ae5868f05ee094a8451ba8dfd25a742a15cb8c3f
neguri <neguri0235@gmail.com>
2021-03-02 00:03:18 +0900

    update

---
+++ algospot/BoardCover.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin>>tc;
+    while(tc--){
+        
+    }
+    return 0;
+}
--- algospot/Picnic.cpp
+++ algospot/Picnic.cpp
@@ -1,20 +1,76 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
-int tc,n,m,p,q;
+int tc,n,m,a,b;
+
+bool areFriends[10][10];
+bool taken[10];
+
+int countPairings2(bool taken[10])
+{
+    bool finished = true;
+    for(int i = 0; i<n; i++){ 
+        if(!taken[i]) finished = false;
+    }
+    if(finished) return 1;
+    int ret = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(!taken[i] && !taken[j] && areFriends[i][j]) {
+                taken[i] = taken[j] = true;
+                ret += countPairings2(taken);
+                taken[i] = taken[j] = false;
+            }
+        }
+    }
+    return ret;
+}
+
+int countPairings(bool taken[10])
+{
+    int firstFree = -1;
+    for(int i = 0; i<n; i++){
+        if(!taken[i]) {
+            firstFree = i; 
+            break;
+        }
+    }
+
+    if(firstFree == -1) return 1;
+    int ret = 0;
+    for(int pairWith = firstFree+1; pairWith < n; ++pairWith){
+        if(!taken[pairWith] && areFriends[firstFree][pairWith]){
+            taken[firstFree] = taken[pairWith] = true;
+            ret += countPairings(taken);
+            taken[firstFree] = taken[pairWith] = false;
+        }
+    }
+    return ret;
+}
 
 int main()
 {
     cin>>tc;
     while(tc--){
         cin>>n>>m;
-        vector<pair<int,int>> a;
+        memset(taken,0,sizeof(taken));
+        memset(areFriends,0,sizeof(areFriends));
         for(int i = 0; i<m; i++){
-            cin>>p>>q;
-            a.push_back(make_pair(p,q));
+            cin>>a>>b;
+            areFriends[a][b] = areFriends[b][a] = true;
         }
+        #ifdef CB
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                cout<<areFriends[i][j]<<' ';
+            }cout<<endl;
+        }
+        #endif
+        cout<<countPairings(taken)<<endl;
     }
 }
