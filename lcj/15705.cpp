eb6eac7cacf8b4a8ed4363396a3b3c99040f16b6
neguri <neguri0235@gmail.com>
2021-04-08 05:26:02 +0900

    lcj

---
+++ lcj/15705.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+#ifdef CB
+    freopen("in.txt","r",stdin);
+#endif
+    
+
+
+
+    return 0;
+}
+
+/*
+ABCD
+5 5
+ACDBE
+ABCED
+ACCEE
+ACHDF
+ACBCE
+*/
