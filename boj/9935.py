857ab42796edb58e4254ddbda512b78e1614db73
Jaeho Choi <neguri0235@gmail.com>
2022-01-30 23:55:38 +0900

    update

--- boj/9935.py
+++ boj/9935.py
@@ -1,35 +1,49 @@
 import sys
+dbg = 1
+if dbg: sys.stdin = open('in.txt','r')
 
-dbg = 0
+a = list(sys.stdin.readline().rstrip())
+b = list(sys.stdin.readline().rstrip())
 
-s1 = list(sys.stdin.readline().rstrip())
-s2 = list(sys.stdin.readline().rstrip())
+if dbg: print(a,'\n', b)
 
-if dbg == 1:
-    print(s1)
-    print(s2)
-
-
-stk = []
-
-for i in range(len(s1)):
-
-    if s1[i] != s2[len(s2)-1]:
-        stk.append(s1[i])
-    else:
-        k = len(s2)-2
-
-        while len(stk):
-            if stk[-1] == s2[k]:
-                stk.pop()
-                k -=1
-            else:
-                break
-if dbg == 1:
-    print(stk)
-if len(stk) == 0:
-   print("FRULA")
+D = [ 1 for i in range(len(a)+1)]
+ans = []
+if len(b) == 1:
+    for e in a:
+        if e != b[0]:
+            ans.append(e)
+elif len(a) < len(b):
+    for e in a:
+        ans.append(e)
 else:
-   print(''.join(stk))
+    stk = []
 
+    for i in range(len(a)):
+        if a[i] == b[0]:
+            stk.append([i,0])
+        else:
+            if len(stk):
+                atop, btop = stk[-1]
+                if dbg: print(atop, btop)
 
+                if a[i] == b[btop+1]:
+                    stk.append([i,btop+1])
+
+                    if (btop + 1) == len(b)-1:
+                        for i in range(len(b)):
+                            atop, btop = stk.pop()
+                            D[atop] = 0
+                else:
+                    stk.clear()
+
+    for i in range(len(a)):
+        if D[i]:
+            ans.append(a[i])
+
+if not len(ans):
+    print('FRULA')
+else:
+    for e in ans:
+        print(e,end='')
+    print()
